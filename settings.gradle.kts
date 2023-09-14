1cd05b221c32cf0ac0d778232a25b5b77ea36d0d
Nick <nelakov24@gmail.com>
2023-09-14 23:38:12 +0400

    Структура проекта

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "ok-marketplace-202308"
+include("m1l1-hello")
+include("m1l2-basic")
+include("m1l3-oop")
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion apply false
+    }
+}
